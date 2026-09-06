64ed2be330273f972f65cd6b71775bc40cbf5dab
Steve Mwangi <mwangisteve764@gmail.com>
2026-09-06 13:03:29 +0300

    Update assignment4.py, remove comments

--- data_types_practice/assignment4.py
+++ data_types_practice/assignment4.py
@@ -1,13 +1,4 @@
 # Practice Question 4: The Digital Receipt
-# Objective: Combine input(), multiple arithmetic operations, and advanced f-string formatting. File: assignment4.py
-# Instructions: Build a point-of-sale system for a local kiosk.
-# 1.	Ask the user for the name of an item, the price (int), and the quantity bought (int).
-# 2.	Calculate the subtotal.
-# 3.	Calculate a discount of 5% on the subtotal (if subtotal is 1000, discount is 50). Hint: multiply by 0.05.
-# 4.	Calculate the vat (16%) on the discounted amount (subtotal - discount).
-# 5.	Calculate the total to be paid.
-# 6.	Print a formatted receipt using f-strings. Format all money values to 2 decimal places and include commas (e.g., Ksh 1,200.00).
-# 7.	Use an empty print() to create a blank line between the item details and the final total.
 
 name = input("Enter item name : ")
 price= int(input("Enter item price : "))
